eea9553675fc5d99dc8f87129c1c806a7dd82ce3
Vidit Jain <viditpatwa04@gmail.com>
2024-08-24 16:37:51 +0530

    frontend v1

---
+++ extension/README.md
@@ -0,0 +1,53 @@
+cropshot
+-------
+Have you ever wanted to tweet a webpage but didnt like the screeshot twitter automatically uses as the image of the link?🤨... at times, which mostly happens... you have no choice but to stick with it😕.
+
+This chrome extension has come to your rescue!
+
+This is a simple chrome extension for creating attractive screenshots from webpages on google chrome browser, then opens a new tab with twitter as the url and composes a new tweet containing the website url and the screenshot you cropped from that page, waiting for your approval...😎.
+![Crop screenshot](https://i.imgur.com/hFVkmqe.png)
+- Uses a consistent default crop size.
+- Makes it easy to adjust crop size in increments of 5px.
+- Easy to highlight an area of the screenshot to the desired area.
+- No need for twitter API Keys or any configuration after installing the extension.
+- This extension does not post any tweets or media automatically, you have control of what you want to tweet, this extension just facilitates it...
+
+## Installation
+
+Clone the repository:
+
+    git clone https://github.com/mashtullah/cropshot.git
+
+Or you can download the zip file here and extract it on your local computer.
+
+Open Chrome settings, browse to "Extensions", make sure the "Developer mode"
+checkbox is selected, and select "Load unpacked extension":
+
+![Load extension](http://i.imgur.com/v69GizR.png)
+
+Browse to the `cropshot` cloned repository directory(folder) and select it to add the
+link to Chrome's main toolbar.
+And there you go!!
+
+## Usage
+
+Open the webpage you want to tweet then 
+1. Click on the cropshot extention`s icon on chrome 
+![cropshot icon](https://i.imgur.com/dHsTEkD.png "Cropshot")
+
+
+2. A new tab will open with a rectangle where you can crop and resize your desired screenshot portion.
+
+3. After you get your perfect shot click Done button at the bottom of the screen.
+
+4. Twitter will be opened with the new tweet box active and with the URL and your selected screenshot.
+
+5. You can and your own text and post the tweet when you are fine with it!😎
+
+incase it shows a blank screenshot the first time, just close that tab and refresh the page then try again.
+
+
+
+
+## License
+ *cropshot* is a truly opensource chrome extension which will always be free under the BSD 3 License. You can feel free to use it for commercial or non commercial purposes. 
---
+++ extension/edit.js
@@ -0,0 +1,158 @@
+var init = true;
+
+var url ="";
+var currTab=null;
+var tOld=null;
+var twitterWin=null;
+chrome.extension && chrome.extension.onMessage.addListener(function(image) {
+  if (init) {
+    init = false;
+	//
+    document.getElementById('base').style.backgroundImage = 'url('+image+')';
+    document.getElementById('cropped').style.backgroundImage = 'url('+image+')';
+    //document.getElementById('popped').style.backgroundImage = 'url('+image+')';
+  } else {
+	   if(image.substring(0,4)=="http")url=image;
+    // Create an empty canvas element
+    var l = parseInt($('#cropped').css('left'), 10);
+    var t = parseInt($('#cropped').css('top'), 10);
+    var w = parseInt($('#cropped').css('width'), 10);
+    var h = parseInt($('#cropped').css('height'), 10);
+
+    var canvas = document.createElement('canvas');
+    canvas.width = w;
+    canvas.height = h;
+
+    // Copy the image contents to the canvas
+    var ctx = canvas.getContext('2d');
+    var img = new Image();
+    img.onload =async function() {
+      ctx.drawImage(img, l, t, w, h, 0, 0, w, h);
+      $('#final')
+        .attr('src', canvas.toDataURL('image/png'))
+        .css({
+          marginLeft: -0.5 * w + 'px', 
+          marginTop: -0.5 * h + 'px'
+        });
+    const url =document.getElementById('final').src;
+		const blobInput = await loadBlob(url);
+		 writeToClipboard(blobInput);
+      $('body').addClass('final');
+    };
+    img.src = image;
+  }
+});
+
+
+async function loadBlob(fileName) {
+  const fetched = await fetch(fileName);
+  return await fetched.blob();
+}
+async function writeToClipboard(imageBlob) {
+  try {
+    await navigator.clipboard.write([
+      new ClipboardItem({
+        'image/png': imageBlob
+      })
+    ]);
+    chrome.tabs.getCurrent(function(tab) {
+      tOld=tab.id;
+    });
+	composeTweet();
+	
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+
+  
+function processResult (res){
+  console.log('Results Received:'+res);
+  setTimeout(function() {closeTab()}, 6000);
+}
+function closeTab(r){
+  chrome.tabs.remove(tOld);
+}
+async function composeTweet() {
+  try {
+   
+		twitterWin=window.open(`https://twitter.com/compose/tweet?text=`+url);
+		$(twitterWin).ready(function()
+		{
+			setTimeout(function() {
+				chrome.tabs.query({active: true}, function(tabs) {
+          var tab = tabs[0];
+          currTab=tabs[0];
+          tab_title = tab.title;
+          //console.log(tab.url+'pasted at:'+tab_title);
+				  chrome.tabs.executeScript(tab.id, {
+					code: 'document.execCommand("paste")'
+				  }, processResult);
+        });					
+				}, 3000);
+		});
+
+		
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+var port = chrome.extension.connect({
+    name: "ClientX"
+});
+port.postMessage("Request URL");
+port.onMessage.addListener(function (msg) {
+    url=msg;
+	//console.log("URL recieved is  " + msg);
+});
+  
+$(function() {
+	
+  $('a[href=#save]').click(function() {
+    $('#toolbar').hide();
+    chrome.extension.sendMessage({ action: 'capture' });
+    return false;
+  });
+  $('a[href=#close]').click(function() {
+    chrome.tabs.getCurrent(function(tab) {
+      chrome.tabs.remove(tab.id);
+    });
+    return false;
+  });
+  $('.image').each(function() {
+    var image = $(this);
+    image
+      .draggable({
+        grid: [5,5],
+        containment: 'document'
+      })
+      .resizable({
+        grid: [5,5],
+        containment: 'document',
+        handles: 'n, e, s, w, ne, se, sw, nw'
+      })
+      .bind('drag', function(event, ui) {
+        var left = ui.offset.left;
+        var top  = ui.offset.top;
+        image.css({
+          backgroundPosition: (left * -1) + 'px ' + (top * -1) + 'px'
+        });
+      })
+      .bind('resize', function(event, ui) {
+        var l = parseInt($(ui.element).css('left'), 10);
+        var t = parseInt($(ui.element).css('top'), 10);
+        var w = parseInt($(ui.element).css('width'), 10);
+        var h = parseInt($(ui.element).css('height'), 10);
+        $(ui.element).css({ backgroundPosition: (l*-1) + 'px ' + (t*-1) + 'px' });
+        $('.dimensions', image).text(w + 'x' + h);
+      });
+  });
+  
+  
+  
+
+  
+});
+
